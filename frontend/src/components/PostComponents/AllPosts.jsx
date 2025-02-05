c7c432cb535c22412f4790540d9ffac517e6bcfc
SpeedyKabal <62118414+SpeedyKabal@users.noreply.github.com>
2025-02-05 18:54:05 +0100

    Implement post editing feature with textarea and update handling; add keyboard escape functionality for canceling edits

--- frontend/src/components/PostComponents/AllPosts.jsx
+++ frontend/src/components/PostComponents/AllPosts.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import api from "../../api";
 import Reactions from "./Reactions";
 import Comments from "./Comments";
@@ -9,10 +9,12 @@ import { useTranslation } from "react-i18next";
 import "../../styles/mediaChrome.css";
 import { useUser } from "../../Contexts/Usercontext";
 import PostDropdown from "./PostDropdown";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 import {
   formatTime,
   contentDisplay,
   convertLinksInsideMessages,
+  adjustTextareaHeight,
 } from "../../services/Utilities";
 import Media from "./Media";
 
@@ -20,6 +22,13 @@ function AllPosts({ post, OnPostDeleted, showDetails = false }) {
   const currentUser = useUser();
   const { t, i18n } = useTranslation();
   const isArabic = i18n.language == "ar";
+  const [updatePostcontent, setUpdatePostContent] = useState(post.content);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    adjustTextareaHeight(textareaRef);
+  }, [updatePostcontent]);
 
   const handleDeletePost = async (id) => {
     await api.delete("api/post/delete/", { data: { post_id: id } });
@@ -29,7 +38,14 @@ function AllPosts({ post, OnPostDeleted, showDetails = false }) {
   };
 
   const handleEditPost = async (id) => {
-    alert("This Feature Will be added soon !");
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        setIsUpdate(false);
+        return;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
   };
 
   return (
@@ -54,19 +70,53 @@ function AllPosts({ post, OnPostDeleted, showDetails = false }) {
             </div>
             {currentUser.id == post.author.id && (
               <PostDropdown
-                onEdit={() => handleEditPost(post.id)}
+                onEdit={() => setIsUpdate(true)}
                 onDelete={() => handleDeletePost(post.id)}
               />
             )}
           </div>
           <Media urlFile={post.mediaFiles} />
           {showDetails ? (
-            <p
-              className="text-gray-800 mb-4 whitespace-pre-line text-xl leading-relaxed ml-2"
-              dir={contentDisplay(post.content) ? "rtl" : "ltr"}
-            >
-              {convertLinksInsideMessages(post.content)}
-            </p>
+            isUpdate ? (
+              <div className="relative">
+                <textarea
+                  ref={textareaRef}
+                  name="updateContent"
+                  value={updatePostcontent}
+                  onChange={(e) => setUpdatePostContent(e.target.value)}
+                  className="resize-none text-xl w-full px-1 py-2 lg:px-4 lg:py-2 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 lg:placeholder:text-xl"
+                ></textarea>
+                <button
+                  onClick={handleEditPost(post.id)}
+                  className="text-blue-500 hover:bg-green-500 rounded-lg absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2 "
+                >
+                  <ChevronRight size={35} strokeWidth={3} absoluteStrokeWidth />
+                </button>
+              </div>
+            ) : (
+              <p
+                className="text-gray-800 mb-4 whitespace-pre-line text-xl leading-relaxed ml-2"
+                dir={contentDisplay(post.content) ? "rtl" : "ltr"}
+              >
+                {convertLinksInsideMessages(post.content)}
+              </p>
+            )
+          ) : isUpdate ? (
+            <div className="relative">
+              <textarea
+                ref={textareaRef}
+                name="updateContent"
+                value={updatePostcontent}
+                onChange={(e) => setUpdatePostContent(e.target.value)}
+                className="resize-none text-xl w-full px-1 py-2 lg:px-4 lg:py-2 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 lg:placeholder:text-xl"
+              ></textarea>
+              <button
+                onClick={handleEditPost(post.id)}
+                className="text-blue-500 hover:bg-green-500 rounded-lg absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2 "
+              >
+                <ChevronRight size={35} strokeWidth={3} absoluteStrokeWidth />
+              </button>
+            </div>
           ) : (
             <Link to={`/post/${post.id}`} className="w-full">
               <p
