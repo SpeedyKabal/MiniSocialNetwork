9c423ddad706bab33b229432d345771511e8476e
speedykabal <khalifa.nourdinzanki@hotmail.com>
2025-07-07 21:21:47 +0100

    update nginx to serve admin page correctly using admin css files

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
     return <div className="flex h-screen flex-wrap justify-center">
-        <h1 className="text-white text-9xl w-full text-center">404 Error</h1>
-        <div className="text-white text-5xl">The Page you are trying to vist is not Found in the server</div>
+        <h1 className="text-9xl w-full text-center">404 Error</h1>
+        <div className="text-5xl">The Page you are trying to vist is not Found in the server</div>
     </div>
 }
 
